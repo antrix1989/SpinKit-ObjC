c473e7fbb5e17343b67597ac46081b938bc026e7
Sergey Demchenko <antrix1989@gmail.com>
2015-08-14 12:34:48 -0700

    Hard code colors for chasing dots animation.

--- SpinKit/Animations/RTSpinKitChasingDotsAnimation.m
+++ SpinKit/Animations/RTSpinKitChasingDotsAnimation.m
@@ -29,7 +29,7 @@
 -(void)setupSpinKitAnimationInLayer:(CALayer*)layer withSize:(CGSize)size color:(UIColor*)color
 {
     NSTimeInterval beginTime = CACurrentMediaTime();
-
+    
     CALayer *spinner = [CALayer layer];
     spinner.frame = CGRectMake(0.0, 0.0, size.width, size.height);
     spinner.anchorPoint = CGPointMake(0.5, 0.5);
@@ -37,37 +37,39 @@
     spinner.shouldRasterize = YES;
     spinner.rasterizationScale = [[UIScreen mainScreen] scale];
     [layer addSublayer:spinner];
-
+    
     CAKeyframeAnimation *spinnerAnim = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
     spinnerAnim.removedOnCompletion = NO;
     spinnerAnim.repeatCount = HUGE_VALF;
     spinnerAnim.duration = 2.0;
     spinnerAnim.beginTime = beginTime;
     spinnerAnim.keyTimes = @[@(0.0), @(0.25), @(0.5), @(0.75), @(1.0)];
-
+    
     spinnerAnim.timingFunctions = @[
-        [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear],
-        [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear],
-        [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear],
-        [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear],
-        [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear]
-    ];
-
+                                    [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear],
+                                    [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear],
+                                    [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear],
+                                    [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear],
+                                    [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear]
+                                    ];
+    
     spinnerAnim.values = @[
-       [NSValue valueWithCATransform3D:CATransform3DMakeRotation(0, 0, 0, 1.0)],
-       [NSValue valueWithCATransform3D:CATransform3DMakeRotation(M_PI_2, 0, 0, 1.0)],
-       [NSValue valueWithCATransform3D:CATransform3DMakeRotation(M_PI, 0, 0, 1.0)],
-       [NSValue valueWithCATransform3D:CATransform3DMakeRotation(3 * M_PI_2, 0, 0, 1.0)],
-       [NSValue valueWithCATransform3D:CATransform3DMakeRotation(2 * M_PI, 0, 0, 1.0)]
-    ];
-
+                           [NSValue valueWithCATransform3D:CATransform3DMakeRotation(0, 0, 0, 1.0)],
+                           [NSValue valueWithCATransform3D:CATransform3DMakeRotation(M_PI_2, 0, 0, 1.0)],
+                           [NSValue valueWithCATransform3D:CATransform3DMakeRotation(M_PI, 0, 0, 1.0)],
+                           [NSValue valueWithCATransform3D:CATransform3DMakeRotation(3 * M_PI_2, 0, 0, 1.0)],
+                           [NSValue valueWithCATransform3D:CATransform3DMakeRotation(2 * M_PI, 0, 0, 1.0)]
+                           ];
+    
     [spinner addAnimation:spinnerAnim forKey:@"spinner-anim"];
+    
+    NSArray *colors = @[[UIColor colorWithRed:96/255.0 green:45/255.0 blue:173/255.0 alpha:1], [UIColor colorWithRed:145/255.0 green:50/255.0 blue:251/255.0 alpha:1]];
     
     for (NSInteger i=0; i < 2; i+=1) {
         CALayer *circle = [CALayer layer];
         CGFloat offset = size.width * 0.3 * i;
         circle.frame = CGRectOffset(CGRectApplyAffineTransform(CGRectMake(0.0, 0.0, size.width, size.height), CGAffineTransformMakeScale(0.6, 0.6)), offset, offset);
-        circle.backgroundColor = color.CGColor;
+        circle.backgroundColor = [colors[i] CGColor];
         circle.anchorPoint = CGPointMake(0.5, 0.5);
         circle.cornerRadius = CGRectGetHeight(circle.bounds) * 0.5;
         circle.transform = CATransform3DMakeScale(0.0, 0.0, 0.0);
@@ -80,17 +82,17 @@
         anim.keyTimes = @[@(0.0), @(0.5), @(1.0)];
         
         anim.timingFunctions = @[
-            [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
-            [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
-            [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]
-        ];
+                                 [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                 [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                 [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]
+                                 ];
         
         anim.values = @[
-            [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)],
-            [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0, 1.0, 0.0)],
-            [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)]
-        ];
-
+                        [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)],
+                        [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0, 1.0, 0.0)],
+                        [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)]
+                        ];
+        
         [spinner addSublayer:circle];
         [circle addAnimation:anim forKey:@"spinkit-anim"];
     }
